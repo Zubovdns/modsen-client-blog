e90d5d973a4caf8d11cc89b5a82deecdd8773806
Monika <zubovdns@icloud.com>
2024-09-13 15:25:42 +0300

    feat: add modal and hook into header and fix 'use client' error

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,32 @@
+"use client";
+import { Video } from "@assets/video";
 import { Button } from "@components/Button";
+import { Modal } from "@components/Modal";
 import { NavLink } from "@components/NavLink";
+import { useModalOpen } from "@hooks/useModalOpen";
 
 import styles from "./header.module.scss";
 
-export const Header = () => (
-  <header className={styles.header}>
-    <h4 className={styles.title}>Modsen Client Blog</h4>
-    <nav className={styles.navbar}>
-      <NavLink href="/">Home</NavLink>
-      <NavLink href="/blog">Blog</NavLink>
-      <NavLink href="/about-us">About Us</NavLink>
-      <NavLink href="/contact-us">Contact us</NavLink>
-    </nav>
-    <Button className={styles.button}>Video about us</Button>
-  </header>
-);
+export const Header = () => {
+  const { isModalOpen, handleModalOpen, handleModalClose } = useModalOpen();
+
+  return (
+    <header className={styles.header}>
+      <h4 className={styles.title}>Modsen Client Blog</h4>
+      <nav className={styles.navbar}>
+        <NavLink href="/">Home</NavLink>
+        <NavLink href="/blog">Blog</NavLink>
+        <NavLink href="/about-us">About Us</NavLink>
+        <NavLink href="/contact-us">Contact us</NavLink>
+      </nav>
+      <Button className={styles.button} onClick={handleModalOpen}>
+        Video about us
+      </Button>
+      {isModalOpen && (
+        <Modal onClose={handleModalClose}>
+          <Video />
+        </Modal>
+      )}
+    </header>
+  );
+};
