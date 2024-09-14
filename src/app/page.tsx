52b659aeb8d4e4e1e40ee320020e4869066c1f46
Monika <zubovdns@icloud.com>
2024-09-14 14:08:32 +0300

    feat: add home-banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import { HomeBanner } from "@components/home-banner/home-banner";
+
 import styles from "./page.module.scss";
 
 export default function Home() {
-  return <div className={styles.page}></div>;
+  return (
+    <div className={styles.page}>
+      <HomeBanner />
+    </div>
+  );
 }
--- src/components/nav-link/nav-link.tsx
+++ src/components/nav-link/nav-link.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Link from "next/link";
 
 import typography from "@styles/typography.module.scss";
