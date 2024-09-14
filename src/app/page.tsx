620d36c5a7e2e7afb4ce58cb4db15301564b8308
Monika <zubovdns@icloud.com>
2024-09-14 19:10:15 +0300

    feat: add about us banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import { HomeAboutUsBanner } from "@components/home-about-us-banner/home-about-us-banner";
+
 import styles from "./page.module.scss";
 
 export default function Home() {
-  return <div className={styles.page}></div>;
+  return (
+    <div className={styles.page}>
+      <HomeAboutUsBanner />
+    </div>
+  );
 }
