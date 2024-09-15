d5707c37850f2d466808856c523d43a0459f0630
Monika <zubovdns@icloud.com>
2024-09-15 15:36:53 +0300

    feat: add localization for join-our-team-banner

--- src/components/join-our-team-banner/constants.ts
+++
@@ -1,4 +0,0 @@
-export const TITLE = "Join our team to be a part of our story";
-export const TEXT =
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.";
-export const BUTTON = "Join now";
--- src/components/join-our-team-banner/join-our-team-banner.tsx
+++ src/components/join-our-team-banner/join-our-team-banner.tsx
@@ -1,13 +1,18 @@
+import { useTranslations } from "next-intl";
+
 import { Button } from "@components/button/button";
 import typography from "@styles/typography.module.scss";
 
-import { BUTTON, TEXT, TITLE } from "./constants";
 import styles from "./join-our-team-banner.module.scss";
 
-export const JoinOurTeamBanner = () => (
-  <div className={styles.banner}>
-    <h4 className={styles.title}>{TITLE}</h4>
-    <p className={`${typography.body01} ${styles.text}`}>{TEXT}</p>
-    <Button className={styles.button}>{BUTTON}</Button>
-  </div>
-);
+export const JoinOurTeamBanner = () => {
+  const t = useTranslations("JoinOurTeam");
+
+  return (
+    <div className={styles.banner}>
+      <h4 className={styles.title}>{t("title")}</h4>
+      <p className={`${typography.body01} ${styles.text}`}>{t("text")}</p>
+      <Button className={styles.button}>{t("button")}</Button>
+    </div>
+  );
+};
