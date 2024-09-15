a3cbb296a2432b1844815749f03fe9486bf6f3da
Monika <zubovdns@icloud.com>
2024-09-15 15:21:36 +0300

    feat: add localization for featured-in-banner

--- src/components/featured-in-banner/featured-in-banner.tsx
+++ src/components/featured-in-banner/featured-in-banner.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { useTranslations } from "next-intl";
 
 import Logo1 from "@public/icons/partners/logo-1.svg";
 import Logo2 from "@public/icons/partners/logo-2.svg";
@@ -6,19 +7,22 @@ import Logo3 from "@public/icons/partners/logo-3.svg";
 import Logo4 from "@public/icons/partners/logo-4.svg";
 import Logo5 from "@public/icons/partners/logo-5.svg";
 
-import { TEXT_BOTTOM, TEXT_TOP } from "./constants";
 import styles from "./featured-in-banner.module.scss";
 
-export const FeaturedInBanner = () => (
-  <div className={styles.container}>
-    <div className={styles.textContainer}>
-      <p className={styles.textTop}>{TEXT_TOP}</p>
-      <h5 className={styles.textBottom}>{TEXT_BOTTOM}</h5>
+export const FeaturedInBanner = () => {
+  const t = useTranslations("FeaturedIn");
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <p className={styles.textTop}>{t("text-top")}</p>
+        <h5 className={styles.textBottom}>{t("text-bottom")}</h5>
+      </div>
+      <Image alt={t("logo-1.alt")} src={Logo1} />
+      <Image alt={t("logo-2.alt")} src={Logo2} />
+      <Image alt={t("logo-3.alt")} src={Logo3} />
+      <Image alt={t("logo-4.alt")} src={Logo4} />
+      <Image alt={t("logo-5.alt")} src={Logo5} />
     </div>
-    <Image alt="Logo 1" src={Logo1} />
-    <Image alt="Logo 2" src={Logo2} />
-    <Image alt="Logo 3" src={Logo3} />
-    <Image alt="Logo 4" src={Logo4} />
-    <Image alt="Logo 5" src={Logo5} />
-  </div>
-);
+  );
+};
