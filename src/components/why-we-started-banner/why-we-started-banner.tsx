9950220ffc43f6f4bf81b6e9224f682dd7791667
Monika <zubovdns@icloud.com>
2024-09-17 21:12:11 +0300

    feat: add 'started' component

--- src/components/why-we-started-banner/why-we-started-banner.tsx
+++ src/components/why-we-started-banner/why-we-started-banner.tsx
@@ -1,5 +1,32 @@
+import Image from "next/image";
+
+import { Button } from "@components/button/button";
+import BannerImage from "@public/images/why-we-started-image.png";
+import typography from "@styles/typography.module.scss";
+
 import styles from "./why-we-started-banner.module.scss";
 
 export const WhyWeStartedBanner = () => (
-  <div className={styles.container}>why-we-started-banner</div>
+  <div className={styles.container}>
+    <div className={styles.imageWrapper}>
+      <Image
+        src={BannerImage}
+        alt="Why we started image"
+        className={styles.image}
+        fill
+      />
+    </div>
+
+    <div className={styles.content}>
+      <p className={styles.header}>Why We Started</p>
+      <div className={styles.text}>
+        <h2>It started out as a simple idea and evolved into our passion</h2>
+        <p className={`${typography.body01}`}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </div>
+      <Button>{"Discover our story >"}</Button>
+    </div>
+  </div>
 );
