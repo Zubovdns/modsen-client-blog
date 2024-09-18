379d602bbc630a0d8ce6060783b5600cbdc1567e
Monika <zubovdns@icloud.com>
2024-09-18 16:44:01 +0300

    feat: add authors

--- src/mocks/authors-mock.ts
+++ src/mocks/authors-mock.ts
@@ -1,12 +1,44 @@
 import { Author } from "@interfaces/author.interface";
 
-export const author_mock: Author = {
-  id: "1",
-  name: "James West",
-  avatar: "https://avatar.iran.liara.run/public",
-  description: "Content Writer @Company",
-  facebook: "https://www.facebook.com",
-  instagram: "https://www.instagram.com/",
-  twitter: "https://x.com/",
-  linkedin: "https://lt.linkedin.com/",
-};
+export const authors_mock: Author[] = [
+  {
+    id: "1",
+    name: "James West",
+    avatar: "https://i.pravatar.cc/1000?u=asdasddasdxda",
+    description: "Content Writer @Company",
+    facebook: "https://www.facebook.com",
+    instagram: "https://www.instagram.com/",
+    twitter: "https://x.com/",
+    linkedin: "https://lt.linkedin.com/",
+  },
+  {
+    id: "2",
+    name: "Dianne Russell",
+    avatar: "https://i.pravatar.cc/1000?u=asdasndkjbnaskjdnaskjdbkasnd",
+    description: "Content Writer @Company",
+    facebook: "https://www.facebook.com",
+    instagram: "https://www.instagram.com/",
+    twitter: "https://x.com/",
+    linkedin: "https://lt.linkedin.com/",
+  },
+  {
+    id: "3",
+    name: "Floyd Miles",
+    avatar: "https://i.pravatar.cc/1000?u=asdasdasddasdxda",
+    description: "Content Writer @Company",
+    facebook: "https://www.facebook.com",
+    instagram: "https://www.instagram.com/",
+    twitter: "https://x.com/",
+    linkedin: "https://lt.linkedin.com/",
+  },
+  {
+    id: "4",
+    name: "Jenny Wilson",
+    avatar: "https://i.pravatar.cc/1000?u=asdhaskjdkas",
+    description: "Content Writer @Company",
+    facebook: "https://www.facebook.com",
+    instagram: "https://www.instagram.com/",
+    twitter: "https://x.com/",
+    linkedin: "https://lt.linkedin.com/",
+  },
+];
