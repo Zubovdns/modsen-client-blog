4e0021380881cf18d719d36fd895e9d039b5d18f
Monika <zubovdns@icloud.com>
2024-09-19 14:27:28 +0300

    feat: add basics for blog-featured-banner

--- src/app/[locale]/blog/page.tsx
+++ src/app/[locale]/blog/page.tsx
@@ -1,3 +1,11 @@
+import { BlogFeaturedBanner } from "@components/blog-featured-banner/blog-featured-banner";
+
+import styles from "../page.module.scss";
+
 export default function Blog() {
-  return <div>Blog</div>;
+  return (
+    <div className={styles.page}>
+      <BlogFeaturedBanner />
+    </div>
+  );
 }
---
+++ src/components/blog-featured-banner/blog-featured-banner.tsx
@@ -0,0 +1,5 @@
+import styles from "./blog-featured-banner.module.scss";
+
+export const BlogFeaturedBanner = () => (
+  <div className={styles.container}></div>
+);
