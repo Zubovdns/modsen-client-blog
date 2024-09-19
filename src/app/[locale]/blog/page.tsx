98e94ff7b25cac498bad5c80146b4a2b53150fac
Monika <zubovdns@icloud.com>
2024-09-19 15:09:43 +0300

    feat: add basics for blog-list-of-posts

--- src/app/[locale]/blog/page.tsx
+++ src/app/[locale]/blog/page.tsx
@@ -1,4 +1,5 @@
 import { BlogFeaturedBanner } from "@components/blog-featured-banner/blog-featured-banner";
+import { BlogListOfPosts } from "@components/blog-list-of-posts/blog-list-of-posts";
 
 import styles from "../page.module.scss";
 
@@ -6,6 +7,7 @@ export default function Blog() {
   return (
     <div className={styles.page}>
       <BlogFeaturedBanner />
+      <BlogListOfPosts />
     </div>
   );
 }
---
+++ src/components/blog-list-of-posts/blog-list-of-posts.tsx
@@ -0,0 +1,9 @@
+import typography from "@styles/typography.module.scss";
+
+import styles from "./blog-list-of-posts.module.scss";
+
+export const BlogListOfPosts = () => (
+  <div className={styles.container}>
+    <h1 className={typography.Heading1}>All posts</h1>
+  </div>
+);
