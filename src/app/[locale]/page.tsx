3ebf3c1940f4fd34a63b75595506bc5e999960c7
Monika <zubovdns@icloud.com>
2024-09-19 10:28:03 +0300

    refactor: change mod to module in home page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -6,42 +6,42 @@ import styles from "./page.module.scss";
 
 const PostsBanner = dynamic(() =>
   import("@components/posts-banner/posts-banner").then(
-    (mod) => mod.PostsBanner,
+    (module) => module.PostsBanner,
   ),
 );
 const HomeAboutUsBanner = dynamic(() =>
   import("@components/home-about-us-banner/home-about-us-banner").then(
-    (mod) => mod.HomeAboutUsBanner,
+    (module) => module.HomeAboutUsBanner,
   ),
 );
 const CategorySelector = dynamic(() =>
   import("@components/category-selector/category-selector").then(
-    (mod) => mod.CategorySelector,
+    (module) => module.CategorySelector,
   ),
 );
 const WhyWeStartedBanner = dynamic(() =>
   import("@components/why-we-started-banner/why-we-started-banner").then(
-    (mod) => mod.WhyWeStartedBanner,
+    (module) => module.WhyWeStartedBanner,
   ),
 );
 const ListOfAuthors = dynamic(() =>
   import("@components/list-of-authors/list-of-authors").then(
-    (mod) => mod.ListOfAuthors,
+    (module) => module.ListOfAuthors,
   ),
 );
 const FeaturedInBanner = dynamic(() =>
   import("@components/featured-in-banner/featured-in-banner").then(
-    (mod) => mod.FeaturedInBanner,
+    (module) => module.FeaturedInBanner,
   ),
 );
 const TestimonialsBanner = dynamic(() =>
   import("@components/testimonials-banner/testimonials-banner").then(
-    (mod) => mod.TestimonialsBanner,
+    (module) => module.TestimonialsBanner,
   ),
 );
 const JoinOurTeamBanner = dynamic(() =>
   import("@components/join-our-team-banner/join-our-team-banner").then(
-    (mod) => mod.JoinOurTeamBanner,
+    (module) => module.JoinOurTeamBanner,
   ),
 );
 
