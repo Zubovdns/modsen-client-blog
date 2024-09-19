ccc925f6872fe02f8f183fea2f40fa0afdafd27d
Monika <zubovdns@icloud.com>
2024-09-19 20:23:40 +0300

    feat: add loader, add smooth scroll after loading, add localization into blog-list-of-authors

--- src/components/blog-list-of-posts/blog-list-of-posts.tsx
+++ src/components/blog-list-of-posts/blog-list-of-posts.tsx
@@ -1,19 +1,22 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import { useTranslations } from "next-intl";
 
 import { getPostsByPage } from "@/service/posts/get-posts-by-page";
+import { Loader } from "@components/loader/loader";
 import { Blog } from "@interfaces/blog.interface";
 import typography from "@styles/typography.module.scss";
 
 import styles from "./blog-list-of-posts.module.scss";
 
+const START_PAGE = 1;
+
 export const BlogListOfPosts = () => {
   const t = useTranslations("PostsBanner");
 
-  const [page, setPage] = useState(1);
+  const [page, setPage] = useState(START_PAGE);
   const [data, setData] = useState<{
     posts: Blog[];
     hasMore: boolean;
@@ -23,12 +26,18 @@ export const BlogListOfPosts = () => {
   });
   const [isLoading, setIsLoading] = useState(true);
 
+  const titleRef = useRef<HTMLHeadingElement | null>(null);
+
   useEffect(() => {
     const fetchPosts = async () => {
       setIsLoading(true);
       const data = await getPostsByPage(page);
+
       setData(data);
       setIsLoading(false);
+      if (titleRef.current) {
+        titleRef.current.scrollIntoView({ behavior: "smooth" });
+      }
     };
     fetchPosts();
   }, [page]);
@@ -41,44 +50,52 @@ export const BlogListOfPosts = () => {
 
   return (
     <div className={styles.container}>
-      <h1 className={`${typography.Heading1} ${styles.title}`}>
+      <h1 ref={titleRef} className={`${typography.Heading1} ${styles.title}`}>
         {t("all-posts.title")}
       </h1>
-      <div className={styles.list}>
-        {data.posts.map(({ category, title, description, title_image, id }) => (
-          <div key={id} className={styles.card}>
-            <div className={styles.imageWrapper}>
-              <Image
-                className={styles.image}
-                alt={`Post image ${id}`}
-                src={title_image}
-                fill
-              />
-            </div>
-            <div className={styles.content}>
-              <p className={typography.cap}>{category}</p>
-              <div className={styles.text}>
-                <h2 className={typography.Heading2}>{title}</h2>
-                <p className={typography.body1}>{description}</p>
+      {isLoading ? (
+        <div className={styles.loaderContainer}>
+          <Loader />
+        </div>
+      ) : (
+        <div className={styles.list}>
+          {data.posts.map(
+            ({ category, title, description, title_image, id }) => (
+              <div key={id} className={styles.card}>
+                <div className={styles.imageWrapper}>
+                  <Image
+                    className={styles.image}
+                    alt={`Post image ${id}`}
+                    src={title_image}
+                    fill
+                  />
+                </div>
+                <div className={styles.content}>
+                  <p className={typography.cap}>{category}</p>
+                  <div className={styles.text}>
+                    <h2 className={typography.Heading2}>{title}</h2>
+                    <p className={typography.body1}>{description}</p>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-        ))}
-      </div>
+            ),
+          )}
+        </div>
+      )}
       <div className={styles.navContainer}>
         <button
-          className={typography.Heading4}
+          className={`${typography.Heading4} ${styles.navButton}`}
           onClick={() => loadPage(page - 1)}
           disabled={page <= 1}
         >
-          {"< Prev"}
+          {t("prev")}
         </button>
         <button
-          className={typography.Heading4}
+          className={`${typography.Heading4} ${styles.navButton}`}
           onClick={() => loadPage(page + 1)}
           disabled={!data.hasMore}
         >
-          {"Next >"}
+          {t("next")}
         </button>
       </div>
     </div>
