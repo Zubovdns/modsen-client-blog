b1b676cd26feb1fcc579dd56b5380ef970a67123
Monika <zubovdns@icloud.com>
2024-09-19 10:00:56 +0300

    feat: add lazy loading for home page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,14 +1,49 @@
-import { CategorySelector } from "@components/category-selector/category-selector";
-import { FeaturedInBanner } from "@components/featured-in-banner/featured-in-banner";
-import { HomeAboutUsBanner } from "@components/home-about-us-banner/home-about-us-banner";
+import dynamic from "next/dynamic";
+
 import { HomeBanner } from "@components/home-banner/home-banner";
-import { JoinOurTeamBanner } from "@components/join-our-team-banner/join-our-team-banner";
-import { ListOfAuthors } from "@components/list-of-authors/list-of-authors";
-import { PostsBanner } from "@components/posts-banner/posts-banner";
-import { TestimonialsBanner } from "@components/testimonials-banner/testimonials-banner";
-import { WhyWeStartedBanner } from "@components/why-we-started-banner/why-we-started-banner";
 
 import styles from "./page.module.scss";
+
+const PostsBanner = dynamic(() =>
+  import("@components/posts-banner/posts-banner").then(
+    (mod) => mod.PostsBanner,
+  ),
+);
+const HomeAboutUsBanner = dynamic(() =>
+  import("@components/home-about-us-banner/home-about-us-banner").then(
+    (mod) => mod.HomeAboutUsBanner,
+  ),
+);
+const CategorySelector = dynamic(() =>
+  import("@components/category-selector/category-selector").then(
+    (mod) => mod.CategorySelector,
+  ),
+);
+const WhyWeStartedBanner = dynamic(() =>
+  import("@components/why-we-started-banner/why-we-started-banner").then(
+    (mod) => mod.WhyWeStartedBanner,
+  ),
+);
+const ListOfAuthors = dynamic(() =>
+  import("@components/list-of-authors/list-of-authors").then(
+    (mod) => mod.ListOfAuthors,
+  ),
+);
+const FeaturedInBanner = dynamic(() =>
+  import("@components/featured-in-banner/featured-in-banner").then(
+    (mod) => mod.FeaturedInBanner,
+  ),
+);
+const TestimonialsBanner = dynamic(() =>
+  import("@components/testimonials-banner/testimonials-banner").then(
+    (mod) => mod.TestimonialsBanner,
+  ),
+);
+const JoinOurTeamBanner = dynamic(() =>
+  import("@components/join-our-team-banner/join-our-team-banner").then(
+    (mod) => mod.JoinOurTeamBanner,
+  ),
+);
 
 export default function Home() {
   return (
