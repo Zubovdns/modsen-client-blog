de103c2d157065409dc4538c9e70cddf1c936702
Monika <zubovdns@icloud.com>
2024-09-20 14:04:11 +0300

    feat: add new variation of button and href in home-banner

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,8 +1,20 @@
+import { Link } from "@i18n/routing";
+
 import styles from "./button.module.scss";
 import { ButtonProps } from "./types";
 
-export const Button = ({ onClick, children, className }: ButtonProps) => (
-  <button className={`${styles.button} ${className}`} onClick={onClick}>
-    {children}
-  </button>
-);
+export const Button = ({ onClick, children, className, href }: ButtonProps) => {
+  if (href) {
+    return (
+      <Link href={href} className={`${styles.button} ${className}`}>
+        {children}
+      </Link>
+    );
+  }
+
+  return (
+    <button className={`${styles.button} ${className}`} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
