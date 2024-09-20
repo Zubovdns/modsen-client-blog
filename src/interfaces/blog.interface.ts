b6126780ea96c19bfbb85e4a0b22da54da6ef17d
Monika <zubovdns@icloud.com>
2024-09-20 13:26:24 +0300

    feat: add text and change text type in posts and blog-post

--- src/interfaces/blog.interface.ts
+++ src/interfaces/blog.interface.ts
@@ -2,16 +2,18 @@ import { Author } from "@interfaces/author.interface";
 
 export type category = "startup" | "business" | "economy" | "technology";
 
+export interface BlogContentBlock {
+  type: "heading" | "paragraph";
+  text: string;
+}
+
 export interface Blog {
   id: string;
-
   author: Author;
   publish_date: Date;
   category: category;
-
   title: string;
   title_image: string;
   description: string;
-
-  content: string;
+  content: BlogContentBlock[];
 }
--- src/mocks/posts-mock.ts
+++ src/mocks/posts-mock.ts
@@ -13,8 +13,40 @@ export const posts_mock: Blog[] = [
       "https://fastly.picsum.photos/id/961/2000/1500.jpg?hmac=xnqH3hZembbq7kLFE8V0Lj-RVw_9MIaMX-XZppB42i8",
     description:
       "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.",
-    content:
-      "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.",
+    content: [
+      {
+        type: "heading",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.",
+      },
+      {
+        type: "paragraph",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non blandit massa enim nec. Scelerisque viverra mauris in aliquam sem. At risus viverra adipiscing at in tellus. Sociis natoque penatibus et magnis dis parturient montes. Ridiculus mus mauris vitae ultricies leo. Neque egestas congue quisque egestas diam. Risus in hendrerit gravida rutrum quisque non.",
+      },
+      {
+        type: "heading",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.",
+      },
+      {
+        type: "paragraph",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non blandit massa enim nec. Scelerisque viverra mauris in aliquam sem. At risus viverra adipiscing at in tellus. Sociis natoque penatibus et magnis dis parturient montes. Ridiculus mus mauris vitae ultricies leo. Neque egestas congue quisque egestas diam. Risus in hendrerit gravida rutrum quisque non.",
+      },
+      {
+        type: "heading",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.",
+      },
+      {
+        type: "paragraph",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non blandit massa enim nec. Scelerisque viverra mauris in aliquam sem. At risus viverra adipiscing at in tellus. Sociis natoque penatibus et magnis dis parturient montes. Ridiculus mus mauris vitae ultricies leo. Neque egestas congue quisque egestas diam. Risus in hendrerit gravida rutrum quisque non.",
+      },
+      {
+        type: "heading",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.",
+      },
+      {
+        type: "paragraph",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non blandit massa enim nec. Scelerisque viverra mauris in aliquam sem. At risus viverra adipiscing at in tellus. Sociis natoque penatibus et magnis dis parturient montes. Ridiculus mus mauris vitae ultricies leo. Neque egestas congue quisque egestas diam. Risus in hendrerit gravida rutrum quisque non.",
+      },
+    ],
   },
   {
     id: "2",
@@ -27,8 +59,40 @@ export const posts_mock: Blog[] = [
       "https://fastly.picsum.photos/id/176/2000/1500.jpg?hmac=SjB0gQR3Khn2q56U-rwKyWynt_VdUq6kzv_FlJBtRwc",
     description:
       "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.",
-    content:
-      "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.",
+    content: [
+      {
+        type: "heading",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.",
+      },
+      {
+        type: "paragraph",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non blandit massa enim nec. Scelerisque viverra mauris in aliquam sem. At risus viverra adipiscing at in tellus. Sociis natoque penatibus et magnis dis parturient montes. Ridiculus mus mauris vitae ultricies leo. Neque egestas congue quisque egestas diam. Risus in hendrerit gravida rutrum quisque non.",
+      },
+      {
+        type: "heading",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.",
+      },
+      {
+        type: "paragraph",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non blandit massa enim nec. Scelerisque viverra mauris in aliquam sem. At risus viverra adipiscing at in tellus. Sociis natoque penatibus et magnis dis parturient montes. Ridiculus mus mauris vitae ultricies leo. Neque egestas congue quisque egestas diam. Risus in hendrerit gravida rutrum quisque non.",
+      },
+      {
+        type: "heading",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.",
+      },
+      {
+        type: "paragraph",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non blandit massa enim nec. Scelerisque viverra mauris in aliquam sem. At risus viverra adipiscing at in tellus. Sociis natoque penatibus et magnis dis parturient montes. Ridiculus mus mauris vitae ultricies leo. Neque egestas congue quisque egestas diam. Risus in hendrerit gravida rutrum quisque non.",
+      },
+      {
+        type: "heading",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.",
+      },
+      {
+        type: "paragraph",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non blandit massa enim nec. Scelerisque viverra mauris in aliquam sem. At risus viverra adipiscing at in tellus. Sociis natoque penatibus et magnis dis parturient montes. Ridiculus mus mauris vitae ultricies leo. Neque egestas congue quisque egestas diam. Risus in hendrerit gravida rutrum quisque non.",
+      },
+    ],
   },
   {
     id: "3",
@@ -40,8 +104,40 @@ export const posts_mock: Blog[] = [
       "https://fastly.picsum.photos/id/176/2000/1500.jpg?hmac=SjB0gQR3Khn2q56U-rwKyWynt_VdUq6kzv_FlJBtRwc",
     description:
       "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.",
-    content:
-      "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.",
+    content: [
+      {
+        type: "heading",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.",
+      },
+      {
+        type: "paragraph",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non blandit massa enim nec. Scelerisque viverra mauris in aliquam sem. At risus viverra adipiscing at in tellus. Sociis natoque penatibus et magnis dis parturient montes. Ridiculus mus mauris vitae ultricies leo. Neque egestas congue quisque egestas diam. Risus in hendrerit gravida rutrum quisque non.",
+      },
+      {
+        type: "heading",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.",
+      },
+      {
+        type: "paragraph",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non blandit massa enim nec. Scelerisque viverra mauris in aliquam sem. At risus viverra adipiscing at in tellus. Sociis natoque penatibus et magnis dis parturient montes. Ridiculus mus mauris vitae ultricies leo. Neque egestas congue quisque egestas diam. Risus in hendrerit gravida rutrum quisque non.",
+      },
+      {
+        type: "heading",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.",
+      },
+      {
+        type: "paragraph",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non blandit massa enim nec. Scelerisque viverra mauris in aliquam sem. At risus viverra adipiscing at in tellus. Sociis natoque penatibus et magnis dis parturient montes. Ridiculus mus mauris vitae ultricies leo. Neque egestas congue quisque egestas diam. Risus in hendrerit gravida rutrum quisque non.",
+      },
+      {
+        type: "heading",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.",
+      },
+      {
+        type: "paragraph",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non blandit massa enim nec. Scelerisque viverra mauris in aliquam sem. At risus viverra adipiscing at in tellus. Sociis natoque penatibus et magnis dis parturient montes. Ridiculus mus mauris vitae ultricies leo. Neque egestas congue quisque egestas diam. Risus in hendrerit gravida rutrum quisque non.",
+      },
+    ],
   },
   {
     id: "4",
@@ -53,8 +149,40 @@ export const posts_mock: Blog[] = [
       "https://fastly.picsum.photos/id/176/2000/1500.jpg?hmac=SjB0gQR3Khn2q56U-rwKyWynt_VdUq6kzv_FlJBtRwc",
     description:
       "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.",
-    content:
-      "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.",
+    content: [
+      {
+        type: "heading",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.",
+      },
+      {
+        type: "paragraph",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non blandit massa enim nec. Scelerisque viverra mauris in aliquam sem. At risus viverra adipiscing at in tellus. Sociis natoque penatibus et magnis dis parturient montes. Ridiculus mus mauris vitae ultricies leo. Neque egestas congue quisque egestas diam. Risus in hendrerit gravida rutrum quisque non.",
+      },
+      {
+        type: "heading",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.",
+      },
+      {
+        type: "paragraph",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non blandit massa enim nec. Scelerisque viverra mauris in aliquam sem. At risus viverra adipiscing at in tellus. Sociis natoque penatibus et magnis dis parturient montes. Ridiculus mus mauris vitae ultricies leo. Neque egestas congue quisque egestas diam. Risus in hendrerit gravida rutrum quisque non.",
+      },
+      {
+        type: "heading",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.",
+      },
+      {
+        type: "paragraph",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non blandit massa enim nec. Scelerisque viverra mauris in aliquam sem. At risus viverra adipiscing at in tellus. Sociis natoque penatibus et magnis dis parturient montes. Ridiculus mus mauris vitae ultricies leo. Neque egestas congue quisque egestas diam. Risus in hendrerit gravida rutrum quisque non.",
+      },
+      {
+        type: "heading",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.",
+      },
+      {
+        type: "paragraph",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non blandit massa enim nec. Scelerisque viverra mauris in aliquam sem. At risus viverra adipiscing at in tellus. Sociis natoque penatibus et magnis dis parturient montes. Ridiculus mus mauris vitae ultricies leo. Neque egestas congue quisque egestas diam. Risus in hendrerit gravida rutrum quisque non.",
+      },
+    ],
   },
 ];
 
