3083e50157f2e6bb5c0b072479c72c8b414d8f75
Monika <zubovdns@icloud.com>
2024-09-23 21:16:08 +0300

    refactor: change type of props for best typification

--- src/app/[locale]/category/[category]/page.tsx
+++ src/app/[locale]/category/[category]/page.tsx
@@ -1,7 +1,7 @@
 import { notFound } from "next/navigation";
 
 import { CategoryBanner } from "@components/category-banner/category-banner";
-import { categories, Category } from "@interfaces/category.interface";
+import { categories } from "@interfaces/category.interface";
 
 import styles from "./page.module.scss";
 import { CategoryPageProps } from "./page.type";
@@ -9,13 +9,13 @@ import { CategoryPageProps } from "./page.type";
 export default function CategoryPage({ params }: CategoryPageProps) {
   const { category } = params;
 
-  if (!Object.values(categories).includes(category as Category)) {
+  if (!Object.values(categories).includes(category)) {
     notFound();
   }
 
   return (
     <div className={styles.page}>
-      <CategoryBanner category={category as Category} />
+      <CategoryBanner category={category} />
     </div>
   );
 }
